14f4a8a52265570868e4b91863adc0542eeb9d30
liyiwei <leeyiw@gmail.com>
2013-04-11 15:49:00 +0800

    rename db to log_mysql

---
+++ src/log_mysql.h
@@ -0,0 +1,28 @@
+/*
+ * inotify-sync - a simple file synchronizer and file system watcher
+ * Copyright (C) 2010-2013, inotify-sync developers and inotify-sync contributors
+ * Copyright (C) 2010-2013, Cohesion Network Security Studio
+ *
+ * inotify-sync is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * inotify-sync is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with inotify-sync; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef _LOG_MYSQL_H
+#define _LOG_MYSQL_H
+
+extern void log_mysql_init();
+extern void log_mysql_event(action_t *action, const char *file,
+	uint32_t cookie);
+
+#endif
